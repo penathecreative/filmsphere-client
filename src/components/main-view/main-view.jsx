2142c9afcb0e9de41f031e55c13b622d795f6ecc
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-03-11 14:18:56 +0100

    Loading Data from API, and Prop Types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,72 +1,26 @@
-import { useState } from "react";
+import { useState, useState } from "react";
 import { MovieView } from "../movie-view/movie-view";
 import { MovieCard } from "../movie-card/movie-card";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: "Gladiator",
-      Description:
-        "A betrayed Roman general seeks justice for his family and revenge against the corrupt emperor who murdered his family and sent him into slavery.",
-      Genre: {
-        Name: "Action",
-        Description:
-          "Action films involve a sequence of events that usually include violence, extended fighting, physical feats, and frantic chases.",
-      },
-      Director: {
-        Name: "Ridley Scott",
-        Bio: "Sir Ridley Scott is an English film director and producer.",
-        Birth: "1937",
-        Death: null,
-      },
-      ImagePath:
-        "https://media.themoviedb.org/t/p/original/9xLIugjmparUuJBuoncNh4jEEaI.jpg",
-      Featured: true,
-    },
-
-    {
-      id: 2,
-      Title: "Silence of the Lambs",
-      Description:
-        "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-      Genre: {
-        Name: "Thriller",
-        Description:
-          "Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience.",
-      },
-      Director: {
-        Name: "Jonathan Demme",
-        Bio: "Robert Jonathan Demme was an American director, producer, and screenwriter.",
-        Birth: "1944",
-        Death: "2017",
-      },
-      ImagePath:
-        "https://image.tmdb.org/t/p/w1280/exSmCOgUM2iXmEpw8AHcQITWDZt.jpg",
-      Featured: true,
-    },
-    {
-      Title: "Inception",
-      Description:
-        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
-      Genre: {
-        Name: "Science Fiction",
-        Description:
-          "Science fiction films explore imaginative and futuristic concepts, often involving space exploration, time travel, and advanced technologies.",
-      },
-      Director: {
-        Name: "Christopher Nolan",
-        Bio: "Christopher Nolan is a British-American film director, producer, and screenwriter.",
-        Birth: "1970",
-        Death: null,
-      },
-      ImagePath:
-        "https://image.tmdb.org/t/p/w1280/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
-      Featured: true,
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://filmsphere-5e594b2ffc50.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie.key,
+            title: movie.Title,
+            genre: movie.Genre.Name,
+            description: movie.Description,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
@@ -84,7 +38,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie._id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
