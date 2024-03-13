bc58e879f39efef569ace16a00e1d12e5a00590a
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-03-13 18:03:11 +0100

    Prop-type commit
    Prop-type structure was changed to attend all the requeriments

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,15 +10,19 @@ export const MainView = () => {
     fetch("https://filmsphere-5e594b2ffc50.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
+        console.log("Movies data:", data);
         const moviesFromApi = data.map((movie) => {
           return {
-            _id: movie._id,
+            _id: movie.key,
             title: movie.Title,
             genre: movie.Genre.Name,
             description: movie.Description,
           };
         });
         setMovies(moviesFromApi);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
       });
   }, []);
 
@@ -38,7 +42,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie._id}
+          key={movie.id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,10 +14,9 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
+
+export default MovieCard;
