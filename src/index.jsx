1efc539f3f793749d8228b7a50cbd9ef87dfc317
penathecreative <149909951+penathecreative@users.noreply.github.com>
2024-03-21 07:16:56 +0100

    React Bootstrap
    -Apply the principles of good design to a React app using UI frameworks
    -Style an app using React Bootstrap features and components

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main compnpmonent (will eventually use all the others)
 const FilmsphereApplication = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 // Finds the root of your app
